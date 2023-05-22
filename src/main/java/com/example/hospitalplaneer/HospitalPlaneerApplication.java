c5588573d9ef8911251e037447aef19b3c08e02d
Panainte Diana <panaintediana29@gmail.com>
2023-05-22 17:19:42 +0300

    Initial commit

---
+++ src/main/java/com/example/hospitalplaneer/HospitalPlaneerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalplaneer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalPlaneerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalPlaneerApplication.class, args);
+    }
+
+}
