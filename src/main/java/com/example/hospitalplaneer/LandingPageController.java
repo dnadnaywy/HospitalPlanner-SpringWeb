197fa58697694e54d492ba86915faac2d1d8e97d
Panainte Diana <panaintediana29@gmail.com>
2023-05-26 13:37:15 +0300

    landing page and doctor's page

---
+++ src/main/java/com/example/hospitalplaneer/LandingPageController.java
@@ -0,0 +1,17 @@
+package com.example.hospitalplaneer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LandingPageController {
+    @GetMapping(value = "/landingPage")
+    public String landingPage() {
+        return "landingPage";
+    }
+
+    @GetMapping(value = "/doctorsPage")
+    public String doctorsPage() {
+        return "doctorsPage";
+    }
+}
